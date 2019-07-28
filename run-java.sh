59268e0bb136a2b39de13b039cd5d15b272b8e93
Henry Feild <hank@feild.org>
2019-07-28 06:29:24 -0400

    Added a script to run any edu.endicott.wei Java program.

---
+++ run-java.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sep=":"
+
+for arg in $@; do 
+    if [ "$arg" == "-s" ]; then
+        defaultConfig=""
+    fi
+done
+
+if [ -d "/c/" ] || [ -d "C:" ]; then 
+    sep=";"
+fi
+
+class=$1; shift
+
+CLASSPATH="" java -cp "bin/${sep}lib/*${sep}book-nlp/book-nlp.jar${sep}book-nlp/lib/*" \
+    edu/endicott/cs/wei/$class $@
