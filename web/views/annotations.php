100141016e2cbaa2347503d666919d331bb54613
Henry Feild <hank@feild.org>
2019-08-06 06:25:06 -0400

    Added automatic annotation options to annotations view.

--- web/views/annotations.php
+++ web/views/annotations.php
@@ -1,7 +1,73 @@
-<h2><em>"<?= $data["text"]["title"] ?>"</em> Annotations</h2>
+<div id="annotations" class="page">
+    <h2><em>"<?= $data["text"]["title"] ?>"</em> Annotations</h2>
+
+    <?php if($user != null){ // Begin logged-in user only section. ?>
+
+    <h3>Creating a new annotation</h3>
+    <p>
+    You can create an annotation in two ways:
+    </p>
+
+    <p>
+    <strong>Method 1: Manual annotation</strong><br/>
+    Click on an annotation below to load it, then click on "Fork" to create a new 
+    annotation based on the one you selected. If you want to start from scratch,
+    click the very first annotation ("blank slate").
+    </p>
+
+    <p>
+    <strong>Method 2: Automatic annotation</strong><br/>
+    Click the button below and select the algorithm you'd like to use. It may take 
+    several minutes for our servers to carry out the annotation, so be patient!
+    </p>
+
+    <!-- File upload. -->
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary btn-md automatic-annotation-button" 
+        data-toggle="modal" data-target="#automatic-annotation-modal">
+    Run automatic annotation
+    </button>
+
+    <div class="modal fade" tabindex="-1" role="dialog" id="automatic-annotation-modal">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <form method="POST" 
+                    action="/texts/<?= $annotation["text_id"] ?>/annotations/<?= $annotation["annotation_id"] ?>">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal"
+                            aria-label="Close"
+                            ><span aria-hidden="true">&times;</span>
+                        </button>
+                        <h4 class="modal-title">Run an automatic annotation</h4>
+
+                    </div>
+                    <div class="modal-body">
+                        <p>
+                            Select the annotation algorithm to run. This may take 
+                            several minutes to process.
+                        </p>
+                        <div class="radio">
+                            <label>
+                                <input type="radio" name="method" value="booknlp"/>
+                                BookNLP (meant for books; entity annotations only)
+                            </label>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" 
+                            data-dismiss="modal">Cancel</button>
+                        <input type="submit" class="btn btn-primary"
+                            value="Run"/>
+                    </div>
+                </form>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
 
 
-<?php 
+    <?php } // End logged-in user only section ?>
+
+<?php
 // Put the annotations in a tree structure based on parent_annotation_id. 
 $annotationNodeLookup = [];
 $annotationTreeRoot = null;
@@ -27,38 +93,41 @@ foreach($data["annotations"] as $annotation){
 }
 ?>
 
-<div id="annotation-list">
-    <ul>
-        <?php 
-            function traverseInOrder($node){
-                $annotation = $node["data"];
-                ?>
-                <li>(<?= $annotation["annotation_id"] ?>) 
-                    [<?= $annotation["method"] ?>] 
-                    <a href="/texts/<?= $annotation["text_id"] ?>/annotations/<?= $annotation["annotation_id"] ?>"
-                    >"<?= ($annotation["label"] == "" ? ("annotation ". $annotation["annotation_id"]) : $annotation["label"]) ?>"</a> 
-                    <?= ($annotation["method"] == "manual" ? (" annotated by ". $annotation["username"]) : "") ?>
-                <?php
-                // TODO -- update this logic to indicate if the root annotation
-                // has been tokenized or not. That's needed to continue...
-                if($annotation["automated_method_error"] === "1")
-                    print " <span class=\"error\">(error processing)</span>";
-                elseif($annotation["automated_method_in_progress"] === "1")
-                    print " <span class=\"note\">(processing...)</span>";
-                
+    <div id="annotation-list">
+        <h3>Annotation List</h3>
 
-                if(count($node["children"]) > 0){
-                    print "<ul>";
-                    foreach($node["children"] as $childNode)
-                        traverseInOrder($childNode);
-                    print "</ul>";
+        <ul>
+            <?php 
+                function traverseInOrder($node){
+                    $annotation = $node["data"];
+                    ?>
+                    <li>(<?= $annotation["annotation_id"] ?>) 
+                        [<?= $annotation["method"] ?>] 
+                        <a href="/texts/<?= $annotation["text_id"] ?>/annotations/<?= $annotation["annotation_id"] ?>"
+                        >"<?= ($annotation["label"] == "" ? ("annotation ". $annotation["annotation_id"]) : $annotation["label"]) ?>"</a> 
+                        <?= ($annotation["method"] == "manual" ? (" annotated by ". $annotation["username"]) : "") ?>
+                    <?php
+                    // TODO -- update this logic to indicate if the root annotation
+                    // has been tokenized or not. That's needed to continue...
+                    if($annotation["automated_method_error"] === "1")
+                        print " <span class=\"error\">(error processing)</span>";
+                    elseif($annotation["automated_method_in_progress"] === "1")
+                        print " <span class=\"note\">(processing...)</span>";
+                    
+
+                    if(count($node["children"]) > 0){
+                        print "<ul>";
+                        foreach($node["children"] as $childNode)
+                            traverseInOrder($childNode);
+                        print "</ul>";
+                    }
+                    print "</li>";
                 }
-                print "</li>";
-            }
 
-            traverseInOrder($annotationTreeRoot);
-        ?>
-    </ul>
+                traverseInOrder($annotationTreeRoot);
+            ?>
+        </ul>
 
+    </div>
 </div>
 
